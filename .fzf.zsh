35c00a27eaa1e9cc1e591068c5f6ef63c7b04fdb
Caetano Sauer <caetanosauer@gmail.com>
2019-10-31 18:55:16 +0100

    use linux home in fzf.zsh

--- .fzf.zsh
+++ .fzf.zsh
@@ -1,14 +1,14 @@
 # Setup fzf
 # ---------
-if [[ ! "$PATH" == */Users/csauer/.vim/bundle/fzf/bin* ]]; then
-  export PATH="$PATH:/Users/csauer/.vim/bundle/fzf/bin"
+if [[ ! "$PATH" == */home/csauer/.vim/bundle/fzf/bin* ]]; then
+  export PATH="$PATH:/home/csauer/.vim/bundle/fzf/bin"
 fi
 
 # Auto-completion
 # ---------------
-[[ $- == *i* ]] && source "/Users/csauer/.vim/bundle/fzf/shell/completion.zsh" 2> /dev/null
+[[ $- == *i* ]] && source "/home/csauer/.vim/bundle/fzf/shell/completion.zsh" 2> /dev/null
 
 # Key bindings
 # ------------
-source "/Users/csauer/.vim/bundle/fzf/shell/key-bindings.zsh"
+source "/home/csauer/.vim/bundle/fzf/shell/key-bindings.zsh"
 
